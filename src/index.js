a76158f793ae2df182374474043e22e0e1e1f5d7
Amin Khan <amin@example.com>
2025-05-31 05:07:45 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+// # ملف .env
+// PUBLIC_URL=/E-commerce
+// REACT_APP_AUTH0_DOMAIN=dev-tbhgrv4m2wdbflhs.us.auth0.com
+// REACT_APP_AUTH0_CLIENT_ID=sPtbGwWiDmAb5RUmtoPiKNOuNeHzcUdS
+// REACT_APP_AUTH0_AUDIENCE=https://dev-tbhgrv4m2wdbflhs.us.auth0.com/api/v2/
+// REACT_APP_AUTH0_SCOPE=openid profile email
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
@@ -13,8 +20,12 @@ root.render(
       <Auth0Provider
         domain={process.env.REACT_APP_AUTH0_DOMAIN}
         clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-
-        // cacheLocation="localstorage"
+        authorizationParams={{
+          redirect_uri: window.location.origin + process.env.PUBLIC_URL,
+          audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+          scope: process.env.REACT_APP_AUTH0_SCOPE
+        }}
+        cacheLocation="localstorage"
       >
         <App />
       </Auth0Provider>
