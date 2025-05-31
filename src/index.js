fdbc1170e3dbed62a24994ebffcde00600c18e46
Amin Khan <amin@example.com>
2025-05-31 04:52:33 +0300

    save

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
 import { BrowserRouter } from 'react-router-dom';
 
