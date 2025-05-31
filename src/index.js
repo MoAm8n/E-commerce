f33badb04a392202da9fab4f73d73323d603342e
Amin Khan <amin@example.com>
2025-05-31 14:10:11 +0300

    save

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
-// # ملف .env
-// PUBLIC_URL=/E-commerce
-// REACT_APP_AUTH0_DOMAIN=dev-tbhgrv4m2wdbflhs.us.auth0.com
-// REACT_APP_AUTH0_CLIENT_ID=sPtbGwWiDmAb5RUmtoPiKNOuNeHzcUdS
-// REACT_APP_AUTH0_AUDIENCE=https://dev-tbhgrv4m2wdbflhs.us.auth0.com/api/v2/
-// REACT_APP_AUTH0_SCOPE=openid profile email
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -18,15 +12,12 @@ root.render(
   <React.StrictMode>
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Auth0Provider
-        domain={process.env.REACT_APP_AUTH0_DOMAIN}
-        clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-        authorizationParams={{
-          redirect_uri: window.location.origin + (process.env.PUBLIC_URL || ''),
-          audience: process.env.REACT_APP_AUTH0_AUDIENCE,
-          scope: process.env.REACT_APP_AUTH0_SCOPE || 'openid profile email'
-        }}
-        cacheLocation="localstorage"
-      >
+          domain="dev-tbhgrv4m2wdbflhs.us.auth0.com"
+          clientId="kuAEWoqdleESgHoq7pulECUlBK3Lw89c"
+          authorizationParams={{
+            redirect_uri: window.location.origin
+          }}
+        >
         <App />
       </Auth0Provider>
     </BrowserRouter>
